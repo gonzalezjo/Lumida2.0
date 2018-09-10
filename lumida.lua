1b77f330ee5c52c04ce2d965407139061255868d
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2018-09-10 19:54:05 -0400

    Scaffolding complete. Just have to test, revise, and actually make useful.

--- lumida.lua
+++ lumida.lua
@@ -14,15 +14,15 @@ do
 
   parser:argument('input', 'Input file.')
 
-  parser:flag('-Nm --no-bytecode', 'Disable bytecode mutation.')
-  parser:flag('-Nt --no-transformations', 'Disable AST transformations.')
+  parser:flag('--no-mutations', 'Disable bytecode mutation.')
+  parser:flag('--no-transformations', 'Disable AST transformations.')
   parser:flag('-R --roblox', 'Enable ROBLOX bytecode specialization.')
   parser:flag('-D --debug', 'Run debug test suite.')
   parser:flag('-V --verbose', 'Enable verbose logging.')
 
   parser:option('-output', 'Output file.', 'out.luac')
   parser:option('-t --transformations', 'Source code transformations.'):args('?')
-  parser:option('-m --mutation', 'Bytecode transformations.'):args('?')
+  parser:option('-m --mutations', 'Bytecode transformations.'):args('?')
 
   arguments = parser:parse()
 end
@@ -33,22 +33,54 @@ do
   obfuscators.bytecode = {}
 end
 
+local source
 do 
   local compiler = require 'compiler'
 
   _G.regular_lua = not arguments.roblox 
 
-  local code_target = (arguments.debug and DEBUG_CODE) or (io.open(input, 'r+b')):('*a')
+  source = arguments.debug and 
+    DEBUG_CODE or 
+    assert(io.open(arguments.input, 'r+b')):read('*a')
 
   local verbose = arguments.verbose
 
-  local output = _G.code_target
-  if arguments.transformations and #arguments.transformations > 0 then 
-    output = _G.code_target
+  if 
+    not arguments.no_transformations and 
+    arguments.transformations and 
+    #arguments.transformations > 0 then 
+
     for _, v in ipairs(arguments.transformations) do 
-      source = obfuscators.ast(_G.verbosity)
+      if not obfuscators.ast[v] then 
+        error('AST obfuscator ' .. v .. ' does not exist.')
+      end 
+
+      source = obfuscators.ast[v](output, verbose)
     end
   end 
 
-  -- if arguments.
+  if 
+    not arguments.no_mutations
+    arguments.mutations and 
+    #arguments.mutations > 0 then 
+    
+    for _, v in ipairs(arguments.transformations) do 
+      if not obfuscators.bytecode[v] then 
+        error('Bytecode obfuscator ' .. v .. ' does not exist.')
+      end 
+
+      source = obfuscators.bytecode[v](output, verbose)
+    end
+  end 
+
+end
+
+do 
+  local f = io.open(arguments.output, 'wb')
+  f:write(source)
+  f:close()
+
+  if arguments.verbose then 
+    print('\nSource code: \n' .. tostring(source))
+  end
 end
