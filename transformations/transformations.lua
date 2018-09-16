13b415c9868400590d574c3fc9611a90d3321661
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2018-09-15 23:08:38 -0400

    Added nodotsyntax.

--- transformations/transformations.lua
+++ transformations/transformations.lua
@@ -1,9 +1,9 @@
 local transformations = {
   noop = assert(require 'transformations/modules/noop'),
-  -- string_comparison = assert(require 'transformations/constant_string_comparison'),
   numerals = assert(require 'transformations/modules/numerals'), 
   strings = assert(require 'transformations/modules/stringencryption'),
   pooling = assert(require 'transformations/modules/pooling'),
+  nodotsyntax = assert(require 'transformations/modules/nodotsyntax')
 } 
 
 return transformations
