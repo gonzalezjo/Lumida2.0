5cf3013c67160acc517e7664539f352bb7c918bb
gonzalezj <gonzalezjo@users.noreply.github.com>
2018-10-25 14:09:45 -0400

    Naming of string encryption module made more consistent.

--- transformations/transformations.lua
+++ transformations/transformations.lua
@@ -1,7 +1,7 @@
 local transformations = {
   noop = assert(require 'transformations/modules/noop'),
   numerals = assert(require 'transformations/modules/numerals'), 
-  strings = assert(require 'transformations/modules/stringencryption'),
+  strings = assert(require 'transformations/modules/strings'),
   pooling = assert(require 'transformations/modules/pooling'),
   nodotsyntax = assert(require 'transformations/modules/nodotsyntax')
 } 
